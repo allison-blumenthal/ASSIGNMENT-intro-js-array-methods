38126458b51d825c38652360674fd0f391676437
Allison Blumenthal <blumen.allison@gmail.com>
2022-10-25 20:44:09 -0400

    coded along with video

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,14 @@ const renderCards = (array) => {
 // .findIndex() & (.includes() - string method)
 const toggleCart = (event) => {
   if (event.target.id.includes("fav-btn")) {
-   console.log('Clicked Fav btn')
+   const [btnText, id] = event.target.id.split('--');
+
+   const index = referenceList.findIndex(item => item.id === Number(id))
+
+   referenceList[index].inCart = !referenceList[index].inCart
+   cartTotal();
+   renderCards(referenceList);
+
   }
 }
 
@@ -27,12 +34,14 @@ const toggleCart = (event) => {
 // .filter()
 const search = (event) => {
   console.log(event);
-  const eventLC = event.target.value.toLowerCase();
-  const searchResult = referenceList.filter(item => {
-    item.title.toLowerCase().includes(eventLC) ||
-    item.author.toLowerCase().includes(eventLC) ||
-    item.description.toLowerCase().includes(eventLC)
-  })
+  const userInput = event.target.value.toLowerCase();
+  const searchResult = referenceList.filter(item => 
+    item.title.toLowerCase().includes(userInput) ||
+    item.author.toLowerCase().includes(userInput) ||
+    item.description.toLowerCase().includes(userInput)
+  )
+
+  renderCards(searchResult);
 }
 
 // BUTTON FILTER
@@ -65,7 +74,7 @@ const buttonFilter = (event) => {
     <tbody>
     `;
     
-    productList().forEach(item => {
+    productList().sort((a, b) => a.type.localeCompare(b.type)).forEach(item => {
       table += tableRow(item);
     });
 
@@ -79,9 +88,19 @@ const buttonFilter = (event) => {
 // CALCULATE CART TOTAL
 // .reduce() & .some()
 const cartTotal = () => {
-  const total = 0
+  const cart = referenceList.filter(item => item.inCart);
+  const total = cart.reduce((value1, value2) => value1 + value2.price, 0);
+  const free = cart.some(item => item.price <= 0);
   document.querySelector("#cartTotal").innerHTML = total.toFixed(2);
+
+  if (free) {
+    document.querySelector('#includes-free').innerHTML = 'INCLUDES FREE ITEMS'
+  } else {
+    document.querySelector('#includes-free').innerHTML = ''
+  }
 }
+
+
 
 // RESHAPE DATA TO RENDER TO DOM
 // .map()
